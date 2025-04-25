e54ab0b9dcb074d82b502bb32e946358ba97de3f
AbdlrhmanDev <144499324+AbdlrhmanDev@users.noreply.github.com>
2025-04-25 03:06:26 +0300

    hii

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isGithubPages = process.env.GITHUB_PAGES === 'true';
-const repo = "medcoachAi"; // ⚠️ غيّره باسم المستودع الحقيقي
+const repo = "medcoachAi"; // 
 const nextConfig = {
-  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
